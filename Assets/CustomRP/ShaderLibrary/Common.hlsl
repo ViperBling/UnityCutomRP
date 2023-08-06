1e851f2addc73a420676279b9cb36169b17deea2
ViperBling <qiusongtao2010@hotmail.com>
2023-08-06 22:44:51 +0800

    [A] 准备ForwardShading

--- Assets/CustomRP/ShaderLibrary/Common.hlsl
+++ Assets/CustomRP/ShaderLibrary/Common.hlsl
@@ -14,5 +14,3 @@
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/UnityInstancing.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
-
-
--- Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
+++ Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
@@ -13,5 +13,3 @@ float4x4 unity_MatrixInvV;
 float4x4 unity_prev_MatrixM;
 float4x4 unity_prev_MatrixIM;
 float4x4 glstate_matrix_projection;
-
-
